0dd573d7b949b99d9bd5c305226909bb84b5caa4
faisalljavid <faisalljavid@gmail.com>
2025-07-20 00:33:38 +0530

    Add layout and functionality for CardClash game, including CSS for styling, HTML structure for card slots and buttons, and JavaScript for deck management and card drawing

---
+++ CardClash/index.js
@@ -0,0 +1,27 @@
+let deckId
+const cardsContainer = document.getElementById("cards")
+const newDeckBtn = document.getElementById("new-deck")
+const drawCardBtn = document.getElementById("draw-cards")
+
+function handleClick() {
+    fetch("https://apis.scrimba.com/deckofcards/api/deck/new/shuffle/")
+        .then(res => res.json())
+        .then(data => {
+            deckId = data.deck_id
+        })
+}
+
+newDeckBtn.addEventListener("click", handleClick)
+
+drawCardBtn.addEventListener("click", () => {
+    fetch(`https://apis.scrimba.com/deckofcards/api/deck/${deckId}/draw/?count=2`)
+        .then(res => res.json())
+        .then(data => {
+            const cardSlots = cardsContainer.children
+            for (let i = 0; i < data.cards.length; i++) {
+                const card = data.cards[i]
+                const cardSlot = cardSlots[i]
+                cardSlot.innerHTML = `<img src="${card.image}" class="card" />`;
+            }
+        })
+})

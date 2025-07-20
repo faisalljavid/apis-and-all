c7346b89450702427856d42a584266c526640cf6
faisalljavid <faisalljavid@gmail.com>
2025-07-20 17:13:22 +0530

    Refactor CSS for improved layout, update HTML structure for crypto display, and enhance JavaScript to fetch and display cryptocurrency data with error handling.

--- VistaSense/index.js
+++ VistaSense/index.js
@@ -4,4 +4,25 @@ fetch("https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&que
         document.body.style.backgroundImage = `url(${data.urls.full})`
         document.getElementById("author").textContent = `By: ${data.user.name}`
     })
+    .catch(err => {
+        // Use a default background image and author
+        document.body.style.backgroundImage = `url(https://images.unsplash.com/photo-1506057213367-028a17ec52e5?crop=entropy&cs=srgb&fm=jpg&ixid=M3wxNDI0NzB8MHwxfHJhbmRvbXx8fHx8fHx8fDE3NTMwMTExNjV8&ixlib=rb-4.1.0&q=85)
+        `
+        document.getElementById("author").textContent = `By: Dan Aragón`
+    })
 
+
+fetch("https://api.coingecko.com/api/v3/coins/dogecoin")
+    .then(res => {
+        if (!res.ok) {
+            throw Error("Something went wrong")
+        }
+        return res.json()
+    })
+    .then(data => {
+        document.getElementById("crypto-top").innerHTML = `
+            <img src=${data.image.small}>
+            <span>${data.name}</span>
+        `
+    })
+    .catch(err => console.error(err))

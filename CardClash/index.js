43f75fca15933108df62af16c11fe7449db2f073
faisalljavid <faisalljavid@gmail.com>
2025-07-20 12:46:54 +0530

    Enhance CardClash game UI and functionality by adding a header for displaying the winner, a section for remaining cards, and updating styles for better layout. Implement winner determination logic in JavaScript.

--- CardClash/index.js
+++ CardClash/index.js
@@ -2,11 +2,16 @@ let deckId
 const cardsContainer = document.getElementById("cards")
 const newDeckBtn = document.getElementById("new-deck")
 const drawCardBtn = document.getElementById("draw-cards")
+const header = document.getElementById("header")
+const remainingText = document.getElementById("remaining-cards")
+
+
 
 function handleClick() {
     fetch("https://apis.scrimba.com/deckofcards/api/deck/new/shuffle/")
         .then(res => res.json())
         .then(data => {
+            remainingText.innerText = `Remaining Cards: ${data.remaining}`
             deckId = data.deck_id
         })
 }
@@ -17,11 +22,45 @@ drawCardBtn.addEventListener("click", () => {
     fetch(`https://apis.scrimba.com/deckofcards/api/deck/${deckId}/draw/?count=2`)
         .then(res => res.json())
         .then(data => {
+            remainingText.innerText = `Remaining Cards: ${data.remaining}`
+
             const cardSlots = cardsContainer.children
             for (let i = 0; i < data.cards.length; i++) {
                 const card = data.cards[i]
                 const cardSlot = cardSlots[i]
                 cardSlot.innerHTML = `<img src="${card.image}" class="card" />`;
             }
+
+            const winnerText = determineWinner(data.cards[0], data.cards[1])
+            header.innerText = winnerText
+
+            if (data.remaining === 0) {
+                drawCardBtn.disabled = true
+            }
         })
 })
+
+const map1 = new Map()
+map1.set("2", 2)
+map1.set("3", 3)
+map1.set("4", 4)
+map1.set("5", 5)
+map1.set("6", 6)
+map1.set("7", 7)
+map1.set("8", 8)
+map1.set("9", 9)
+map1.set("10", 10)
+map1.set("JACK", 11)
+map1.set("QUEEN", 12)
+map1.set("KING", 13)
+map1.set("ACE", 14)
+
+function determineWinner(card1, card2) {
+    if (map1.get(card1.value) > map1.get(card2.value)) {
+        return "Computer Wins!"
+    } else if (map1.get(card1.value) < map1.get(card2.value)) {
+        return "You Win!"
+    } else {
+        return "War!"
+    }
+}

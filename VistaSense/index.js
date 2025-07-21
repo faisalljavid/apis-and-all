78c9c05b8b18b450ea5233bffe0fa675e5cf8f6b
faisalljavid <faisalljavid@gmail.com>
2025-07-21 22:10:52 +0530

    Enhance VistaSense project by updating CSS for improved layout, restructuring HTML to include a weather section, and expanding JavaScript functionality to fetch and display weather data alongside cryptocurrency information.

--- VistaSense/index.js
+++ VistaSense/index.js
@@ -24,5 +24,38 @@ fetch("https://api.coingecko.com/api/v3/coins/dogecoin")
             <img src=${data.image.small}>
             <span>${data.name}</span>
         `
+        document.getElementById("crypto").innerHTML += `
+            <p>🎯: $${data.market_data.current_price.usd}</p>
+            <p>👆: $${data.market_data.high_24h.usd}</p>
+            <p>👇: $${data.market_data.low_24h.usd}</p>
+        `
     })
     .catch(err => console.error(err))
+
+
+
+
+setInterval(() => {
+    const date = new Date()
+    const currentTime = date.toLocaleTimeString("en-us", { timeStyle: "short" })
+    document.querySelector(".time").innerText = currentTime
+}, 1000)
+
+navigator.geolocation.getCurrentPosition(position => {
+    fetch(`https://apis.scrimba.com/openweathermap/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&units=metric`)
+        .then(res => {
+            if (!res.ok) {
+                throw Error("Weather data not available")
+            }
+            return res.json()
+        })
+        .then(data => {
+            const iconUrl = `http://openweathermap.org/img/wn/${data.weather[0].icon}.png`
+            document.getElementById("weather").innerHTML = `
+                <img src=${iconUrl} />
+                <p class="weather-temp">${Math.round(data.main.temp)}º</p>
+                <p class="weather-city">${data.name}</p>
+            `
+        })
+        .catch(err => console.error(err))
+});
